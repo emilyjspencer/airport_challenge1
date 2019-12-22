c6313ff4081dbadd1317831e66fbb5529a2e747c
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-22 12:36:40 +0000

    Airport responds to land and take_off methods

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,8 +3,18 @@ require 'airport'
 describe Airport do
   let(:airport) { Airport.new } 
 
-  it 'can land a plane at the aiport' do
-    expect(airport).to respond_to(:land).with(1).argument 
+  describe '#land' do
+
+    it 'can land a plane at the airport' do
+      expect(airport).to respond_to(:land).with(1).argument 
+    end 
+
+  end 
+
+  describe '#take_off' do
+    it 'allows a plane to take off from the airport' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end 
   end 
 
 
